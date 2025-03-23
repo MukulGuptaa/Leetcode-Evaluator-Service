5689fa3680548b39433c5d6867e61a75b582cf60
Mukul Gupta <mukulgupta@Mukuls-MacBook-Air.local>
2025-03-23 17:25:26 +0530

    Added eslint config

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express from "express";
+
 import serverConfig from "./config/serverConfig";
+
 const app = express();
 app.listen(serverConfig.PORT, () => {
     console.log(`Started server using typescript at ${serverConfig.PORT}`);
