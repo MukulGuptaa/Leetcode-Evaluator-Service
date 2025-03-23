48b589717ff99905c1682088fe0eea0dadab89b8
Mukul Gupta <mukulgupta@Mukuls-MacBook-Air.local>
2025-03-23 17:04:56 +0530

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import express from "express";
+import serverConfig from "./config/serverConfig";
+const app = express();
+app.listen(serverConfig.PORT, () => {
+    console.log(`Started server using typescript at ${serverConfig.PORT}`);
+});
